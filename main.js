213a88ae1d94074c607b658e206b8c8729a930ca
Ramses <nouromar6611@gmail.com>
2025-04-09 02:19:58 +0200

    Initialize the generator

---
+++ main.js
@@ -0,0 +1,34 @@
+let geneBtn = document.querySelector(".gene");
+let autoBtn = document.querySelector(".auto");
+let stopBtn = document.querySelector(".stop");
+let disQuote = document.querySelector(".quote-display");
+let genebtn = document.querySelector(".gene");
+let autoSt = document.querySelector(".auto-status");
+let interHand;
+
+async function getQo() {
+    let res = await fetch("qoutes.json");
+    let data = await res.json()
+    return data
+}
+
+async function geneQo() {
+    let qots = await getQo();
+    let qot = qots[Math.floor(Math.random()* qots.length)]
+    disQuote.innerHTML = qot.text
+}
+
+function autoGene() {
+    interHand = setInterval(geneQo,3000)
+    autoSt.style.visibility = "visible"
+    autoSt.innerHTML ="اوتوماتيكي"
+}
+
+function stopAuto() {
+    clearInterval(interHand)
+    autoSt.style.visibility = "hidden"
+}
+
+geneBtn.onclick = geneQo;
+stopBtn.onclick = stopAuto;
+autoBtn.onclick = autoGene;
